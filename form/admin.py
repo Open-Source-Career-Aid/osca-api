1f2eb74312119381a4868534ec393d585e89d095
maayami <mayank16meena@gmail.com>
2021-06-27 15:09:59 +0530

    Add migrations and update form/admin.py

--- form/admin.py
+++ form/admin.py
@@ -2,10 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Skill)
-admin.site.register(Topic)
-admin.site.register(Subtopic)
-admin.site.register(Resource)
-admin.site.register(Tag)
-admin.site.register(Prerequisite)
+admin.site.register(
+    [User, Skill, Topic, Subtopic, Resource, Tag, Prerequisite, Super_skill])
